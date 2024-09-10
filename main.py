1c8723e9060d7ea75f79c5fb5190941fc14a127b
Ksandr-MTB <sasha.sapegin.99@gmail.com>
2024-09-10 20:50:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,24 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+numbers=[]
+for i in range(1,16):
+    numbers.append(i)
+primes=[]
+not_primes=[]
+for i in range(len(numbers)):
+    if numbers[i]!=1:
+        q=0
+        for j in range(2,numbers[i]):
+            k=numbers[i]%j
+            if k==0:
+                q=q+1
+        if q==0:
+            primes.append(numbers[i])
+        else:
+            not_primes.append(numbers[i])
+    else:
+        continue
+print(primes)
+print(not_primes)
