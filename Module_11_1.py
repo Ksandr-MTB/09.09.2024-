035732b8780b8702f10020fc36a85c4d4d55c450
user <sasha.sapegin.99@gmail.com>
2024-12-01 18:16:53 +0700

    09.09.2024 Znakomstvo s GIT

--- Module_11_1.py
+++ Module_11_1.py
@@ -21,3 +21,34 @@ qw.show()
 print(qw.format, qw.size, qw.mode)
 import numpy
 
+image_filenames = ['obrazec_1.jpg', 'obrazec_black_white.jpg']
+images = [Image.open(i) for i in image_filenames]
+images[0].save('gif_1.gif', save_all=True, append_images=images[1:], duration=500,
+               loop = 0)
+qwe = open('gif_1.gif')
+# qwe.show()
+# print(qwe.format, qwe.size, qwe.mode)
+
+import matplotlib.pyplot
+import random
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [1, 3, 5, 7, 9, 10, 7, 5, 3, 1]
+x_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y_1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# for i in range(10):
+#     x.append(random.randint(1, 10))
+#     y.append(random.randint(1, 10))
+fig, ax = matplotlib.pyplot.subplots()
+ax.set_title('Какой-то график')
+ax.set_xlabel('Кордината горизонтальная Х')
+ax.set_ylabel('Кордината вертикальная У')
+ax.grid()
+
+
+
+ax.plot(x, y)
+ax.plot(y_1, x_1)
+
+print(f'{x}\n{y}')
+matplotlib.pyplot.show()
